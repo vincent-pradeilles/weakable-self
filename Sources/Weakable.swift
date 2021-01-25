1e956aa66172d207b692cf03e9015825500a6917
Clément Nonn <clement.nonn@worldline.com>
2021-01-25 10:32:37 +0100

    add possibility to use it with completion that returns something by providing a default value

--- Sources/Weakable.swift
+++ Sources/Weakable.swift
@@ -13,68 +13,99 @@ public protocol Weakable: class { }
 extension NSObject: Weakable { }
 
 public extension Weakable {
-    
-    public func weakify(_ code: @escaping (Self) -> Void) -> () -> Void {
+    func weakify<ReturnType>(defaultValue: ReturnType, _ code: @escaping (Self) -> ReturnType) -> () -> ReturnType {
         return { [weak self] in
-            guard let self = self else { return }
-            
-            code(self)
+            guard let self = self else { return defaultValue }
+
+            return code(self)
         }
     }
-    
-    public func weakify<A>(_ code: @escaping (Self, A) -> Void) -> (A) -> Void {
+
+    func weakify(_ code: @escaping (Self) -> Void) -> () -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A>(defaultValue: ReturnType, _ code: @escaping (Self, A) -> ReturnType) -> (A) -> ReturnType {
         return { [weak self] a in
-            guard let self = self else { return }
-            
-            code(self, a)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a)
         }
     }
-    
-    public func weakify<A, B>(_ code: @escaping (Self, A, B) -> Void) -> (A, B) -> Void {
+
+    func weakify<A>(_ code: @escaping (Self, A) -> Void) -> (A) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B>(defaultValue: ReturnType, _ code: @escaping (Self, A, B) -> ReturnType) -> (A, B) -> ReturnType {
         return { [weak self] a, b in
-            guard let self = self else { return }
-            
-            code(self, a, b)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b)
         }
     }
     
-    public func weakify<A, B, C>(_ code: @escaping (Self, A, B, C) -> Void) -> (A, B, C) -> Void {
+    func weakify<A, B>(_ code: @escaping (Self, A, B) -> Void) -> (A, B) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B, C>(defaultValue: ReturnType, _ code: @escaping (Self, A, B, C) -> ReturnType) -> (A, B, C) -> ReturnType {
         return { [weak self] a, b, c in
-            guard let self = self else { return }
-            
-            code(self, a, b, c)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b, c)
         }
     }
     
-    public func weakify<A, B, C, D>(_ code: @escaping (Self, A, B, C, D) -> Void) -> (A, B, C, D) -> Void {
+    func weakify<A, B, C>(_ code: @escaping (Self, A, B, C) -> Void) -> (A, B, C) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B, C, D>(defaultValue: ReturnType, _ code: @escaping (Self, A, B, C, D) -> ReturnType) -> (A, B, C, D) -> ReturnType {
         return { [weak self] a, b, c, d in
-            guard let self = self else { return }
-            
-            code(self, a, b, c, d)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b, c, d)
         }
     }
-    
-    public func weakify<A, B, C, D, E>(_ code: @escaping (Self, A, B, C, D, E) -> Void) -> (A, B, C, D, E) -> Void {
+
+    func weakify<A, B, C, D>(_ code: @escaping (Self, A, B, C, D) -> Void) -> (A, B, C, D) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B, C, D, E>(defaultValue: ReturnType, _ code: @escaping (Self, A, B, C, D, E) -> ReturnType) -> (A, B, C, D, E) -> ReturnType {
         return { [weak self] a, b, c, d, e in
-            guard let self = self else { return }
-            
-            code(self, a, b, c, d, e)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b, c, d, e)
         }
     }
-    
-    public func weakify<A, B, C, D, E, F>(_ code: @escaping (Self, A, B, C, D, E, F) -> Void) -> (A, B, C, D, E, F) -> Void {
+
+    func weakify<A, B, C, D, E>(_ code: @escaping (Self, A, B, C, D, E) -> Void) -> (A, B, C, D, E) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B, C, D, E, F>(defaultValue: ReturnType, _ code: @escaping (Self, A, B, C, D, E, F) -> ReturnType) -> (A, B, C, D, E, F) -> ReturnType {
         return { [weak self] a, b, c, d, e, f in
-            guard let self = self else { return }
-            
-            code(self, a, b, c, d, e, f)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b, c, d, e, f)
         }
     }
-    
-    public func weakify<A, B, C, D, E, F, G>(_ code: @escaping (Self, A, B, C, D, E, F, G) -> Void) -> (A, B, C, D, E, F, G) -> Void {
+
+    func weakify<A, B, C, D, E, F>(_ code: @escaping (Self, A, B, C, D, E, F) -> Void) -> (A, B, C, D, E, F) -> Void {
+        return weakify(defaultValue: (), code)
+    }
+
+    func weakify<ReturnType, A, B, C, D, E, F, G>(defaultValue: ReturnType, _ code: @escaping (Self, A, B, C, D, E, F, G) -> ReturnType) -> (A, B, C, D, E, F, G) -> ReturnType {
         return { [weak self] a, b, c, d, e, f, g in
-            guard let self = self else { return }
-            
-            code(self, a, b, c, d, e, f, g)
+            guard let self = self else { return defaultValue }
+
+            return code(self, a, b, c, d, e, f, g)
         }
+    }
+
+    func weakify<A, B, C, D, E, F, G>(_ code: @escaping (Self, A, B, C, D, E, F, G) -> Void) -> (A, B, C, D, E, F, G) -> Void {
+        return weakify(defaultValue: (), code)
     }
 }
